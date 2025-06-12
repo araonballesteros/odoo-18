e34e809852721e8dfce740e0da015cbcca5cf338
araonballesteros <araon.ballesteros@gmail.com>
2025-06-11 21:58:03 -0500

    Add sequence to TourBooking and ignore pycache

--- custom-addons/tour_booking/models/tour_booking.py
+++ custom-addons/tour_booking/models/tour_booking.py
@@ -1,8 +1,14 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class TourBooking(models.Model):
     _name = 'tour.booking'
     _description = 'Reserva de Paquete Turístico'
 
-    name = fields.Char(string='Referencia', required=True)
+    name = fields.Char(string='Referencia', required=True, default='New')
     notes = fields.Text(string='Notas')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code('tour.booking') or '/'
+        return super().create(vals)
